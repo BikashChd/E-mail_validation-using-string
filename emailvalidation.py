5c88f98844e0de946e7e82fb84b5ed19ac7552df
Bikash Chaudhary <bikashchy346@gmail.com>
2022-07-26 21:56:27 +0545

    email validation

---
+++ emailvalidation.py
@@ -0,0 +1,34 @@
+email=input("Enter your email:")
+b=0
+c=0
+d=0
+if len(email)>=6:  #1
+    if email[0].isalpha():  #2
+        if ("@" in email) and (email.count("@")==1):  #3
+            if (email[-3]==".") ^ (email[-4]=="."):  #4
+                for a in email:
+                    if a==a.isspace():
+                        b=1
+                    elif a.isalpha():
+                        if a==a.upper():
+                            c=1
+                    elif a.isdigit():
+                        continue
+                    elif a=="_" or a=="." or a=="@":
+                        continue
+                    else:
+                        d=1
+
+
+                if b==1 or c==1 or d==1:  #5
+                    print("incorrect email 5")
+                else:
+                    print("you entered correct email")
+            else:
+                print("incorrect email 4")
+        else:
+            print("incorrect email  3")
+    else:
+        print("Wrong email 2")
+else:
+    print("Incorrect email 1")
